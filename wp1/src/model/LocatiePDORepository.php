f626cd3dbc31e403f6a02bd1b332fd68a44fd1a1
Maurits Seelen <mauritsseelen@hotmail.com>
2017-09-29 19:44:05 +0200

    locatierepo added

--- wp1/src/model/LocatiePDORepository.php
+++ wp1/src/model/LocatiePDORepository.php
@@ -33,22 +33,57 @@ class LocatiePDORepository implements LocatieRepository
 
     public function getById(int $id)
     {
-        // TODO: Implement getById() method.
+        try {
+            $statement = $this->connection->prepare("SELECT * FROM locatie WHERE id=?");
+            $statement->bindParam(1, $id, \PDO::PARAM_INT);
+            $statement->setFetchMode(\PDO::FETCH_ASSOC);
+            $statement->execute();
+            $locatie = $statement->fetchAll();
+            return $locatie;
+        } catch (\Exception $exception) {
+            return null;
+        }
     }
 
     public function addLocatie(Locatie $locatie)
     {
-        // TODO: Implement addLocatie() method.
+        try {
+            $id = $locatie->getId();
+            $naam = $locatie->getNaam();
+            $statement = $this->connection->prepare('INSERT INTO locatie(id, naam) VALUES(?,?)');
+            $statement->bindParam(1, $id, \PDO::PARAM_INT);
+            $statement->bindParam(2, $naam, \PDO::PARAM_STR);
+            $statement->execute();
+            return $locatie;
+        } catch (\Exception $exception) {
+            return null;
+        }
     }
 
     public function updateLocatie(Locatie $locatie)
     {
-        // TODO: Implement updateLocatie() method.
+        try {
+            $id = $locatie->getId();
+            $naam = $locatie->getNaam();
+
+            $statement = $this->connection->prepare('UPDATE locatie SET naam=? WHERE id=?');
+            $statement->bindParam(1, $naam, \PDO::PARAM_STR);
+            $statement->bindParam(2, $id, \PDO::PARAM_INT);
+            $statement->execute();
+        } catch (\Exception $exception) {
+            return null;
+        }
     }
 
     public function deleteLocatie(int $id)
     {
-        // TODO: Implement deleteLocatie() method.
+        try {
+            $statement = $this->connection->prepare("DELETE * FROM locatie WHERE id=?");
+            $statement->bindParam(1, $id, \PDO::PARAM_INT);
+            $statement->execute();
+        } catch (\Exception $exception) {
+            return null;
+        }
     }
 
 }
