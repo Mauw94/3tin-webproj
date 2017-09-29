8dd66f3f7bd44f82bd13031e30bc1a3d1f232269
Maurits Seelen <mauritsseelen@gmail.com>
2017-09-29 09:33:10 +0200

    initial commit2

--- wp1/src/model/locatiePDORepository.php
+++ wp1/src/model/locatiePDORepository.php
@@ -1,15 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Maurits
- * Date: 29-9-2017
- * Time: 09:06
- */
 
 namespace model;
 
 
-class locatiePDORepository
+class locatiePDORepository implements locatieRepository
 {
+    public function getById(int $id)
+    {
+        // TODO: Implement getById() method.
+    }
+
+    public function addLocatie(Locatie $locatie)
+    {
+        // TODO: Implement addLocatie() method.
+    }
+
+    public function updateLocatie(Locatie $locatie)
+    {
+        // TODO: Implement updateLocatie() method.
+    }
+
+    public function deleteLocatie(int $id)
+    {
+        // TODO: Implement deleteLocatie() method.
+    }
 
 }
