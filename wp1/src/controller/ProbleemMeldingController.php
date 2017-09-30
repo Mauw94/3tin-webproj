f6f0d0c77cda78f57932e9c191d8d6370d8ba02f
Yannick <yannick.bloemen@student.pxl.be>
2017-09-30 10:07:21 +0200

    Probleem melding controller afmaken + clean code check

--- wp1/src/controller/ProbleemMeldingController.php
+++ wp1/src/controller/ProbleemMeldingController.php
@@ -9,6 +9,7 @@
 namespace controller;
 
 
+use model\ProbleemMelding;
 use model\ProbleemMeldingPDORepository;
 
 class ProbleemMeldingController
@@ -19,14 +20,37 @@ class ProbleemMeldingController
         $this->view = $view;
     }
 
-    public function handleGetAll() {
+    public function handleGetAll()
+    {
         $probleemMelding = $this->probleemMeldingRepo->getAll();
         $this->view->show(['toShow' => $probleemMelding]);
     }
 
-    public function handleGetById(int $id) {
+    public function handleGetById(int $id)
+    {
         $probleemMelding = $this->probleemMeldingRepo->getById($id);
         $this->view->show(['toShow' => $probleemMelding]);
     }
 
+    public function handleUpdateProbleemMelding($probleemMelding)
+    {
+        $decode = json_decode($probleemMelding, true);
+        $newProbleemMelding = new ProbleemMelding((int)$decode['id'], $decode['locatieid'], $decode['probleem'], $decode['datum'], $decode['afgehandeld']);
+        $this->probleemMeldingRepo->updateProbleemmelding($newProbleemMelding);
+        $this->view->show(['toShow' => $newProbleemMelding]);
+    }
+
+    public function handleAddProbleemMelding($probleemMelding)
+    {
+        $decode = json_decode($probleemMelding, true);
+        $newProbleemMelding = new ProbleemMelding(0, $decode['locatieid'], $decode['probleem'], $decode['datum'], $decode['afgehandeld']);
+        $this->probleemMeldingRepo->addProbleemMelding($newProbleemMelding);
+        $this->view->show(['toShow' => $newProbleemMelding]);
+    }
+
+    public function handleGetAfgehandeldeProbleemMeldingen()
+    {
+        $afgehandeldeProbleemMeldingen = $this->probleemMeldingRepo->getAfgehandeldeProbleemMeldingen();
+        $this->view->show(['toShow' => $afgehandeldeProbleemMeldingen]);
+    }
 }
