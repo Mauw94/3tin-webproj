fbf3470269f0865896c8af623b60792bae55f589
Maurits Seelen <mauritsseelen@gmail.com>
2017-10-05 16:43:40 +0200

    unit test for locatiePDOrepo

--- wp1/test/model/LocatiePDORepositoryTest.php
+++ wp1/test/model/LocatiePDORepositoryTest.php
@@ -3,7 +3,7 @@
 use \model\LocatiePDORepository;
 use \model\Locatie;
 
-class LocatiePDORepositoryTest extends PHPUnit_Framework_TestCase
+class LocatiePDORepositoryTest extends \PHPUnit_Framework_TestCase
 {
     private $pdoLocatieRepository;
 
@@ -19,12 +19,36 @@ class LocatiePDORepositoryTest extends PHPUnit_Framework_TestCase
         $this->pdoLocatieRepository = null;
     }
 
-    public function testFindLocatieById()
+    public function test_FindLocatieById_found()
     {
         $locatie = $this->pdoLocatieRepository->getById(0);
         $testLocatie = new Locatie(0, "Test");
         $this->assertEquals($locatie, $testLocatie);
     }
 
+    public function test_findLocatieById_nothing()
+    {
+        $locatie = $this->pdoLocatieRepository->getById(-1);
+        $this->assertEquals($locatie, null);
+    }
+
+    public function test_findLocatieById_error()
+    {
+        $locatie = $this->pdoLocatieRepository->getById("a");
+        $this->assertEquals($locatie, null);
+    }
+
+    public function test_added_error()
+    {
+        $locatie = new Locatie("a", "b");
+        $return = $this->pdoLocatieRepository->addLocatie($locatie);
+        $this->assertNull($return);
+    }
+
+    public function test_delete_error()
+    {
+        $return = $this->pdoLocatieRepository->deleteLocatie("a");
+        $this->assertNull($return);
+    }
 
 }
