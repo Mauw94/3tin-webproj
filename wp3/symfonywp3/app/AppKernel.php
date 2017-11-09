824d196cb1d7d2fa166ade26d4ab0d5d276e216e
Yannick <yannick.bloemen@student.pxl.be>
2017-11-09 16:31:15 +0100

    View picture

--- wp3/symfonywp3/app/AppKernel.php
+++ wp3/symfonywp3/app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
 
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
 
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
